84ad4a68260d480a2ba55289ee70094099aa6195
crazy0629 <milan.jansen0629@gmail.com>
2023-07-04 05:22:55 +0800

    update: profile page with api integration

--- src/modules/profile/transactions/index.tsx
+++ src/modules/profile/transactions/index.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TransactionActionWrapper, TransactionsWrapper } from "./styles";
 import { ClaimsTableWrapper } from "../claims/styles";
 import { transactionsData } from "./data";
 import { Button } from "../../../components";
+import { getTransactions } from "../../../actions";
 
 export const TransactionsSection: React.FC = () => {
+  const [tableData, setTableData] = useState([]);
+  useEffect(() => {}, []);
+
+  const getData = async () => {
+    const resData = await getTransactions();
+    if (resData.success) {
+      setTableData(resData.data);
+    } else {
+      setTableData([]);
+    }
+  };
+
   return (
     <TransactionsWrapper>
       <h2>Transactions</h2>
@@ -17,20 +30,28 @@ export const TransactionsSection: React.FC = () => {
           </tr>
         </thead>
         <tbody>
-          {transactionsData.map((item, key) => (
-            <tr key={key}>
-              <td>{item.date}</td>
-              <td>
-                <span className={item.amount < 0 ? "amount-cell" : ""}>
-                  {item.amount < 0 && "-"}$
-                  {item.amount >= 0 ? item.amount : item.amount * -1}
-                </span>
-              </td>
-              <td>
-                <span>{item.type}</span>
+          {tableData.length > 0 ? (
+            tableData.map((item: any, key: number) => (
+              <tr key={key}>
+                <td>{new Date(item.created_at).toDateString()}</td>
+                <td>
+                  <span className={item.amount < 0 ? "amount-cell" : ""}>
+                    {item.amount < 0 && "-"}$
+                    {item.amount >= 0 ? item.amount : item.amount * -1}
+                  </span>
+                </td>
+                <td>
+                  <span>{item.description}</span>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={3} style={{ textAlign: "center" }}>
+                No Data
               </td>
             </tr>
-          ))}
+          )}
         </tbody>
       </ClaimsTableWrapper>
       <TransactionActionWrapper>
