27c3d69c98907bfb2001c471bb3d2dfc17f4a540
pandoratoolbox <30897811+pandoratoolbox@users.noreply.github.com>
2023-07-02 20:07:05 -0500

    fix axios baseUrl

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: "",
+  baseURL: "http://194.5.152.57:3333",
   headers: {
     "Content-Type": "application/json",
   },
