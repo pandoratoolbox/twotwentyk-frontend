b6cbf306fb587dc18cbca374f2f3e0b683922c4e
pandoratoolbox <30897811+pandoratoolbox@users.noreply.github.com>
2023-10-07 13:42:56 +0700

    remove extra errors

--- src/modules/marketplace/MBuyCardSection.tsx
+++ src/modules/marketplace/MBuyCardSection.tsx
@@ -81,7 +81,7 @@ export const MBuyCardSection: React.FC<CardSidebarProps> = ({
       }
     } catch (e: any) {
       console.log(e);
-      toast.error(e.response.data);
+      // toast.error(e.response.data);
     }
   };
 
